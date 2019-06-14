c3ab445abfe603df4adffadc18f32d861344e280
Ariel D <adboy316@yahoo.com>
2019-06-14 21:34:00 +0200

    first background iteration

---
+++ static/animate.js
@@ -0,0 +1,34 @@
+// Wrap every letter in a span
+function main(){
+   
+
+
+
+$('.ml11 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+   
+  })
+  .add({
+    
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: function(el, i) {
+      return 34 * (i+1)
+    }
+  }).add({
+    targets: '.ml11',
+    
+  
+  });
+
+}
+
+document.addEventListener("DOMContentLoaded", main);
