00e8a8f3f5fab4dea3ed4f278a40f8d3b0589840
Ariel D <adboy316@yahoo.com>
2019-06-17 13:11:02 +0200

    added animation for ellipses in svg onClick

--- static/main.js
+++ static/main.js
@@ -1,3 +1,37 @@
-window.addEventListener("DOMContentLoaded", function() {
-    // do stuff
-}, false);
+var timesClicked = 0;
+
+function keyAnimations() {
+
+    if (timesClicked > 1) {
+        return;
+    }
+
+    if (timesClicked === 0) {
+        timesClicked++;
+        let ellipse1 = document.getElementById('path4698');
+    ellipse1.style.animationPlayState  = 'running';
+    document.querySelector('form-control').onclick = () => {
+        if (ellipse1.style.animationPlayState  === 'paused')
+        ellipse1.style.animationPlayState = 'running';
+        else
+        ellipse1.style.animationPlayState  = 'paused';
+    }
+
+    
+    } else {
+
+        let ellipse1 = document.getElementById('ellipse4728');
+        ellipse4728.style.animationPlayState  = 'running';
+    document.querySelector('form-control').onclick = () => {
+        if (ellipse4728.style.animationPlayState  === 'paused')
+        ellipse4728.style.animationPlayState = 'running';
+        else
+        ellipse4728.style.animationPlayState  = 'paused';
+    }
+
+
+    }
+    
+}
+
+
